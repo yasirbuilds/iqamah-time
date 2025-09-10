29bda2a2702888738386a9c4f8d775b877e8f0c8
muhammadyasir7070 <yasir@bigosoft.us>
2025-09-11 00:09:33 +0500

    postgresql database connected

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,19 @@
 import express from "express";
+import pool from "./config/db";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello from Express + TypeScript!");
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json({ time: result.rows[0] });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Database connection error");
+  }
 });
 
 app.listen(PORT, () => {
